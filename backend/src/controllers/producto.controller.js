88bb86e740663fd5c519a1d1e0a4c71b230571f1
Yeimer Jaramillo <andresjara0897@hotmail.com>
2025-07-03 15:16:26 -0500

    Backend: Se agrega setTimeout al endpoint de obtención de productos con paginación para simular una base de datos más grande.

--- backend/src/controllers/producto.controller.js
+++ backend/src/controllers/producto.controller.js
@@ -1,23 +1,25 @@
 const service = require('../services/producto.service');
 
 exports.obtenerTodosPaginacion = async (req, res) => {
-  try {
-    const { nombre, categoria, pagina = 1, cantidadRegistros = 10 } = req.query;
+  setTimeout(async () => {
+    try {
+      const { nombre, categoria, pagina = 1, cantidadRegistros = 10 } = req.query;
 
-    const data = await service.obtenerTodosPaginacion(
-      nombre,
-      categoria,
-      pagina,
-      parseInt(cantidadRegistros)
-    );
-    res.json(data);
-  } catch (error) {
-    console.error('Error al obtener productos:', error);
-    res.status(500).json({
-      ok: false,
-      message: 'Error al obtener productos',
-    });
-  }
+      const data = await service.obtenerTodosPaginacion(
+        nombre,
+        categoria,
+        pagina,
+        parseInt(cantidadRegistros)
+      );
+      res.json(data);
+    } catch (error) {
+      console.error('Error al obtener productos:', error);
+      res.status(500).json({
+        ok: false,
+        message: 'Error al obtener productos',
+      });
+    }
+  }, 1500);
 };
 
 exports.crear = async (req, res) => {
